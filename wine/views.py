4c527020be24f9c7fa910f88da061b7f95fae6db
mik <mail@kissel.ch>
2020-03-21 00:05:13 +0100

    implement dealer field

--- wine/views.py
+++ wine/views.py
@@ -23,7 +23,7 @@ class EditView(UpdateView):
     model = Wine
     template_name = 'wine/wine_form.html'
     fields = ['winename', 'producer', 'grapes', 'year', 'country',
-              'region', 'purchase', 'notes', 'drinkfrom', 'drinkto', 'nmbrbottles']
+              'region', 'purchase', 'dealer', 'notes', 'drinkfrom', 'drinkto', 'nmbrbottles']
     success_url = reverse_lazy('wine:wine_list')
 
 

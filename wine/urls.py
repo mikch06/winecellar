4de73b22ce46bc5d440c3bc14534d949677bfdad
mik <mail@kissel.ch>
2020-05-05 23:04:00 +0200

    revision auth

--- wine/urls.py
+++ wine/urls.py
@@ -8,11 +8,10 @@ app_name = 'wine'
 urlpatterns = [
     path('wine/', views.WinesView.as_view(), name='wine_list'),
     path('wine/<int:pk>', views.DetailView.as_view(), name='wine_detail'),
-    path('edit/<int:pk>', views.EditView.as_view(), name='wine_edit'),
-    path('delete/<int:pk>', views.DeleteView.as_view(), name='wine_delete'),
+    path('wine/edit/<int:pk>', views.EditView.as_view(), name='wine_edit'),
+    path('wine/delete/<int:pk>', views.DeleteView.as_view(), name='wine_delete'),
     path('new/', views.CreateView.as_view(), name='wine_create'),
 
     path('about/', views.about, name='about'),
-    #path('', views.home, name='index'), remove startpage
     path('', views.WinesView.as_view(), name='wine_list'),
 ]

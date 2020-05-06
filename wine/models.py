b6dd07b68d2dabb1287c95400ae244684f453e3a
mki <mail@kissel.ch>
2020-05-06 17:31:53 +0200

    introduce choice for country

--- wine/models.py
+++ wine/models.py
@@ -2,14 +2,28 @@ import datetime
 from django.db import models
 
 class Wine(models.Model):
+    COUNTRY = [
+        ('x', 'x'),
+        ('Australien', 'Australien'),
+        ('Frankreich', 'Frankreich'),
+        ('Deutschland', 'Deutschland'),
+        ('Italien', 'Italien'),
+        ('Österreich', 'Österreich'),
+        ('Portugal', 'Portugal'),
+        ('Schweiz', 'Schweiz'),
+        ('Spanien', 'Spanien'),
+        ('Südafrika', 'Südafrika'),
+        ('USA', 'USA'),
+    ]
+
     winename = models.CharField(max_length=200, blank=True)
     producer = models.CharField(max_length=200, blank=True)
     grapes = models.CharField(max_length=200, blank=True,)
     year = models.CharField(max_length=200, blank=True)
-    country = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=12, choices=COUNTRY)
     region = models.CharField(max_length=200, blank=True)
     purchase = models.DateField(blank=True, null=True)
-    price = models.FloatField (blank=True, default=0)
+    price = models.FloatField(blank=True, default=0)
     dealer = models.CharField(max_length=200, blank=True)
     drinkfrom = models.CharField(max_length=200, blank=True)
     drinkto = models.CharField(max_length=200, blank=True)

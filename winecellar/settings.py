6ef1b169c463cacfc9f3472452ad049190c35bbd
mik <mail@kissel.ch>
2020-05-08 18:08:07 +0200

    split config settings.py settings_dev.py

--- winecellar/settings.py
+++ winecellar/settings.py
@@ -70,17 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'winecellar.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -119,3 +108,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
+
+from winecellar.settings_dev import *

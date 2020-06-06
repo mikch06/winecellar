1fc5bf22eb29c90d3a95d4e5d16ed31cb6dd0bca
mik <mail@kissel.ch>
2020-06-06 15:47:30 +0200

    go back to running login

--- wine/views.py
+++ wine/views.py
@@ -24,7 +24,7 @@ class WinesView(LoginRequiredMixin, generic.ListView):
     # Filter user data only
     def get_queryset(self):
         query_set = super().get_queryset()
-        return query_set.filter(owner__user=self.request.user)
+        return query_set.filter(owner=self.request.user)
 
 
 

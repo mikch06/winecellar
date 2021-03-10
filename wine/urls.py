db52db44b22e7b0707b1af23ddb674dc37ec1165
mik <mail@kissel.ch>
2021-03-10 22:51:35 +0100

    Edit uri for new wine -> /wine/new

--- wine/urls.py
+++ wine/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index),
     path('wine/', views.WinesView.as_view(), name='wine_list'),
     path('wine/delete/<int:pk>', views.DeleteView.as_view(), name='wine_delete'),
-    path('new/', views.createWine, name='create_wine'),
+    path('wine/new/', views.createWine, name='create_wine'),
     path('wine/edit/<int:pk>', views.updateWine, name='update_wine'),
     path('about/', views.about),
     path('wine/full/', views.FullView.as_view(), name='wine_fullview'),

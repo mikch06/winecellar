455b5ef74258f5cdcc0f9bc9c2196e157becdda8
mik <mail@kissel.ch>
2021-03-30 23:33:32 +0200

    commit

--- wine/views.py
+++ wine/views.py
@@ -76,15 +76,14 @@ def copyWine(request, pk):
     form = WineForm(instance=update)
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = WineForm(request.POST)
+        form = WineForm(request.POST, instance=update)
         # Create instance for user data entry
         if form.is_valid():
             instance = form.save(commit=False)
             instance.owner = request.user
             instance.save()
             return HttpResponseRedirect('/wine')
-    else:
-        form = WineForm()
+
     return render(request, 'wine/create_form.html', {'form': form})
 
 class FullView(LoginRequiredMixin, generic.ListView):

a56dda9be077f76ab38e4112f64411fea13e83d7
mikch0 <mail@kissel.ch>
2021-04-29 11:20:33 +0200

    Working search

--- wine/views.py
+++ wine/views.py
@@ -120,4 +120,12 @@ class WineLog(LoginRequiredMixin, generic.ListView):
 class SearchResultsView(generic.ListView):
     model = Wine
     template_name = 'wine/search.html'
-    queryset = Wine.objects.filter(country__icontains='Schweiz')
+
+    def get_queryset(self):  # new
+        query = self.request.GET.get('q')
+        object_list = Wine.objects.filter(
+            Q(country__icontains=query) | Q(producer__icontains=query)|
+            Q(region__icontains=query) | Q(year__icontains=query)|
+            Q(notes__icontains=query)
+        )
+        return object_list

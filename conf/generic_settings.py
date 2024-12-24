e889b141d2eb1fde7d62782bbaa801575083de50
mik <tools@kissel.ch>
2024-12-24 15:47:34 +0100

    insert dev key

--- conf/generic_settings.py
+++ conf/generic_settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-uz_7)9yr4w$b^esulypwo_-(rf1*#im3c%iy%hb5w&8r8acd11"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

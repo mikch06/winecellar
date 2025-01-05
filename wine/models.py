dc200793cefcf881535147537158054b6487e5b1
mik <tools@kissel.ch>
2025-01-05 16:33:43 +0000

    implement datepicker, revert range slider for number of bottles

--- wine/models.py
+++ wine/models.py
@@ -96,7 +96,5 @@ class WineForm(ModelForm):
             'drinkfrom': forms.NumberInput(attrs={'class': "form-control", 'min': '2000', 'max': '2060'}),
             'drinkto': forms.NumberInput(attrs={'class': "form-control", 'min': '2018', 'max': '2060', 'type': 'number'}),
             'warehouse': forms.TextInput(attrs={'class': "form-control"}),
-            'nmbrbottles': forms.NumberInput(attrs={'class': "form-range", 'data-bs-toggle': 'tooltip', 'title': '{{nmbrbottles}}', 'type': 'range', 'step': '1', 'min': '0', 'max': '18'}),
-
-
+            'nmbrbottles': forms.NumberInput(attrs={'class': "form-control"}),
         }

64b8962cc45c21589467be9a603a1cf72b847242
mik <tools@kissel.ch>
2025-05-30 17:20:13 +0200

    add logout url

--- wine/urls.py
+++ wine/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('wine/full/', views.FullView.as_view(), name='wine_fullview'),
     path('wine/log/', views.WineLog.as_view(), name='wine_log'),
     path('wine/detail/<int:pk>', views.WineLogDetail, name='wine_log_detail'),
+    path('logout/', logout_view, name='logout'),
 
     #path('charts/', views.EditorChartView.as_view(), name='charts')
 ]

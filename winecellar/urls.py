9af5a84cfb213f2364a2f83f9fa5b5e0b3568739
mik <tools@kissel.ch>
2025-10-17 15:40:06 +0000

    typ fix

--- winecellar/urls.py
+++ winecellar/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-
     path('', include('wines.urls')),
     #path('admin/', admin.site.urls),
 ]

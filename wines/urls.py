13cea253b3c912ff9e65a06e641bc09426734f4b
mik <tools@kissel.ch>
2025-10-19 16:03:50 +0000

    url cleanup

--- wines/urls.py
+++ wines/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 from .views import *
+from django.contrib.auth import views as auth_views
 
 # App Namespace for app 'wine'
 # !remove this namespace
 urlpatterns = [
-    #path('wine/', views.WinesView.as_view(), name='wine_list'),
-    #path('wine/delete/<int:pk>', views.DeleteView.as_view(), name='wine_delete'),
-    #path('wine/new/', views.createWine, name='create_wine'),
-    #path('wine/edit/<int:pk>', views.updateWine, name='update_wine'),
     #TODO: copy View
     #path('wine/copy/<int:pk>', views.copyWine, name='copy_wine'),
     path('about/', views.about),
@@ -22,13 +19,12 @@ urlpatterns = [
     path('wine/detail/<int:pk>', views.WineLogDetail, name='wine_log_detail'),
     path('logout/', views.logout_view),
 
-
     # New urls routes
-    path("", WineListView.as_view(), name="wine_list"),
+    path('', views.index),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path("wines/", WineListView.as_view(), name="wine_list"),
     path("<int:pk>/", WineDetailView.as_view(), name="wine_detail"),
     path("<int:pk>/edit/", WineUpdateView.as_view(), name="wine_edit"),
     path("new/", WineCreateView.as_view(), name="wine_new"),
     path("wines/<int:pk>/delete/", wine_delete, name="wine_delete"),
-
-    #path('charts/', views.EditorChartView.as_view(), name='charts')
 ]

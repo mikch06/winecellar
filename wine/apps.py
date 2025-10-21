aa1f954d38fc7417e05b3d532aff6bc74ab9b020
mik <tools@kissel.ch>
2025-10-21 20:29:45 +0000

    rename

--- wine/apps.py
+++
@@ -1,8 +0,0 @@
-# from django.apps import AppConfig
-# from watson import search as watson
-#
-# class WineConfig(AppConfig):
-#     name = 'wine'
-#     def ready(self):
-#         Wine = self.get_model("Wine")
-#         watson.register(Wine)

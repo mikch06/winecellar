836df654577e0f20fbcdd220d8d80d6cf766eb9f
mik <mail@kissel.ch>
2025-11-09 14:05:27 +0100

    reorder all routes

--- wines/urls.py
+++ wines/urls.py
@@ -3,30 +3,20 @@ from . import views
 from .views import *
 from django.contrib.auth import views as auth_views
 
-# App Namespace for app 'wine'
-
-
-
-# !remove this namespace
 urlpatterns = [
-    path('about/', views.about),
-    path('info/', views.info),
-    path('wine/export/', views.export_csv),
-    path('wine/export_xls/', views.export_xls),
-    #TODO: Fix as_view
-    #path('wine/full/', views.FullView.as_view(), name='wine_fullview'),
-    path('wines/log/', views.WineLog.as_view(), name='wine_log'),
-    path('wine/detail/<int:pk>', views.WineLogDetail, name='wine_log_detail'),
-    path('logout/', views.logout_view),
-
-    # New urls routes
     path('', views.index),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path("wines/", WineListView.as_view(), name="wine_list"),
-    path("<int:pk>/", WineDetailView.as_view(), name="wine_detail"),
-    path("<int:pk>/edit/", WineUpdateView.as_view(), name="wine_edit"),
+    path("wines/<int:pk>/", WineDetailView.as_view(), name="wine_detail"),
+    path("wines/<int:pk>/edit/", WineUpdateView.as_view(), name="wine_edit"),
     path("new/", WineCreateView.as_view(), name="wine_new"),
     path("wines/<int:pk>/delete/", wine_delete, name="wine_delete"),
     path('wines/<int:pk>/copy/', views.copyWine, name='wine_copy'),
     path('wines/debug/', lambda request: HttpResponse("DEBUG OK")),
+    path('about/', views.about),
+    path('info/', views.info),
+    path('wine/export/', views.export_csv),
+    path('wine/export_xls/', views.export_xls),
+    path('wines/log/', views.WineLog.as_view(), name='wine_log'),
+    path('logout/', views.logout_view),
 ]
